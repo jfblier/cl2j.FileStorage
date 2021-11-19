fb64e9d96fba52bc07064f8d4a37fae779c4f27f
Jean-Francois Blier <jfblier@locketgo.com>
2021-11-19 04:49:30 -0500

    Moved files directly under the root folder instead of src.

---
+++ cl2j.FileStorage/Extensions/JsonExtensions.cs
@@ -0,0 +1,45 @@
+﻿using cl2j.FileStorage.Core;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace cl2j.FileStorage.Extensions
+{
+    public static class JsonExtensions
+    {
+        public static string SerializeJsonObject<T>(T obj, bool indented = false)
+        {
+            var options = new JsonSerializerOptions()
+            {
+                WriteIndented = indented,
+                PropertyNameCaseInsensitive = true
+            };
+            return JsonSerializer.Serialize(obj, options);
+        }
+
+        public static T DeserializeJsonObject<T>(string json)
+        {
+            var options = new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            return JsonSerializer.Deserialize<T>(json, options);
+        }
+
+        public static async Task<T> ReadJsonObjectAsync<T>(this IFileStorageProvider fileStorageProvider, string fileName, Encoding encoding = null) where T : new()
+        {
+            var data = await fileStorageProvider.ReadTextAsync(fileName, encoding);
+
+            if (string.IsNullOrWhiteSpace(data))
+                return new T();
+
+            return DeserializeJsonObject<T>(data);
+        }
+
+        public static async Task WriteJsonObjectAsync<T>(this IFileStorageProvider fileStorageProvider, string fileName, T obj, bool indented = false, Encoding encoding = null)
+        {
+            var data = SerializeJsonObject(obj, indented);
+            await fileStorageProvider.WriteTextAsync(fileName, data, encoding);
+        }
+    }
+}
