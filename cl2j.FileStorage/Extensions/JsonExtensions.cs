6b68aab636030de76f528679286f89b7db98ff2b
Jean-Francois Blier <jfblier@gmail.com>
2022-01-09 16:40:07 -0500

    Changed to use Newtonsoft for TypeHandling

--- cl2j.FileStorage/Extensions/JsonExtensions.cs
+++ cl2j.FileStorage/Extensions/JsonExtensions.cs
@@ -1,49 +1,51 @@
 ﻿using cl2j.FileStorage.Core;
+using Newtonsoft.Json;
 using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace cl2j.FileStorage.Extensions
 {
     public static class JsonExtensions
     {
-        private static readonly JsonSerializerOptions optionsDeserialize = new();
-
-        static JsonExtensions()
-        {
-            optionsDeserialize.PropertyNameCaseInsensitive = true;
-            optionsDeserialize.Converters.Add(new JsonStringEnumConverter());
-        }
-
-        public static string SerializeJsonObject<T>(T obj, bool indented = false)
-        {
-            var options = new JsonSerializerOptions()
-            {
-                WriteIndented = indented,
-                PropertyNameCaseInsensitive = true
-            };
-            return JsonSerializer.Serialize(obj, options);
-        }
-
-        public static T? DeserializeJsonObject<T>(string json)
-        {
-            return JsonSerializer.Deserialize<T>(json, optionsDeserialize);
-        }
-
-        public static async Task<T?> ReadJsonObjectAsync<T>(this IFileStorageProvider fileStorageProvider, string fileName, Encoding? encoding = null) where T : new()
+        public static async Task<T> ReadJsonObjectAsync<T>(this IFileStorageProvider fileStorageProvider, string fileName, Encoding? encoding = null, bool specifyType = false) where T : new()
         {
             var data = await fileStorageProvider.ReadTextAsync(fileName, encoding);
-
             if (string.IsNullOrWhiteSpace(data))
                 return new T();
 
-            return DeserializeJsonObject<T>(data);
+            var options = CreateSerializerOptions(specifyType);
+
+            var value = JsonConvert.DeserializeObject<T>(data, options);
+            if (value == null)
+                throw new Exception($"Unable to deserialize {typeof(T).Name} from value '{data}'");
+            return value;
         }
 
-        public static async Task WriteJsonObjectAsync<T>(this IFileStorageProvider fileStorageProvider, string fileName, T obj, bool indented = false, Encoding? encoding = null)
+        public static async Task WriteJsonObjectAsync<T>(this IFileStorageProvider fileStorageProvider, string fileName, T obj, bool indented = false, Encoding? encoding = null, bool specifyType = false)
         {
-            var data = SerializeJsonObject(obj, indented);
+            var data = SerializeJsonObject(obj, indented, specifyType);
             await fileStorageProvider.WriteTextAsync(fileName, data, encoding);
+        }
+
+        private static string SerializeJsonObject<T>(T obj, bool indented, bool specifyType)
+        {
+            var options = CreateSerializerOptions(specifyType);
+            options.Formatting = indented ? Formatting.Indented : Formatting.None;
+
+            return JsonConvert.SerializeObject(obj, options);
+        }
+
+        private static JsonSerializerSettings CreateSerializerOptions(bool specifyType)
+        {
+            var options = new JsonSerializerSettings()
+            {
+                DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+                DefaultValueHandling = DefaultValueHandling.Ignore,
+                NullValueHandling = NullValueHandling.Ignore,
+            };
+            options.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
+            if (specifyType)
+                options.TypeNameHandling = TypeNameHandling.Objects;
+            return options;
         }
     }
 }
